b33df2fb07fb6ac075c1f2fd4b027ff8ba54f502
judoree <135936771+judoree@users.noreply.github.com>
2024-12-01 17:06:20 +0900

    Code Delete

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,6 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,46 +23,13 @@ class _AppState extends State<App> {
         ),
       ),
       home: const Scaffold(
-        // backgroundColor: const Color(0xFF181818),
+        backgroundColor: Color(0xFF181818),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  int counter = 0;
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        color: Theme.of(context).textTheme.titleLarge?.color,
-        fontSize: 40,
       ),
     );
   }
