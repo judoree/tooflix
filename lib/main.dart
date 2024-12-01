0bd4655a5d17b9634d3e9a595359c85a934abf8f
judoree <135936771+judoree@users.noreply.github.com>
2024-12-01 15:18:44 +0900

    State Create button

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,53 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void ocClicked() {
+    counter = counter + 1;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xFF181818),
-        body: Container(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click me',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                ),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                ),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: ocClicked,
+                icon: const Icon(
+                  Icons.add_alarm_rounded,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
