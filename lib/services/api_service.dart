3a6747dd8bfbc3231ebc256643cb77ae636b234f
judoree <135936771+judoree@users.noreply.github.com>
2024-12-04 18:16:22 +0900

    JsonDecode

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,17 +1,24 @@
 //이 file 안에는 flutter 위젯이나 클래스가 아닌 평범한 클래스 만들어줌
 
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:tooflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     throw Error;
   }
