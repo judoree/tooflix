102f99c628d4d8c03bc28006b13fbe3fa6b6d591
judoree <135936771+judoree@users.noreply.github.com>
2024-12-05 18:46:16 +0900

    ApiService

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,6 +3,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:tooflix/models/webtoon_detail_model.dart';
+import 'package:tooflix/models/webtoon_episode_model.dart';
 import 'package:tooflix/models/webtoon_model.dart';
 
 class ApiService {
@@ -22,5 +24,30 @@ class ApiService {
       return webtoonInstances;
     }
     throw Error;
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var epsoide in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(epsoide));
+      }
+      return episodesInstances;
+    }
+    throw Error();
   }
 }
