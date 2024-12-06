196a8012f55ab3cc9d327aa4431972638065c4e3
judoree <135936771+judoree@users.noreply.github.com>
2024-12-06 17:13:12 +0900

    Futures Detail

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:tooflix/models/webtoon_detail_model.dart';
+import 'package:tooflix/models/webtoon_episode_model.dart';
+import 'package:tooflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
+
   const DetailScreen({
     super.key,
     required this.title,
     required this.thumb,
     required this.id,
   });
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +37,7 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w600,
@@ -35,7 +53,7 @@ class DetailScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Hero(
-                tag: id,
+                tag: widget.id,
                 child: Container(
                   width: 250,
                   clipBehavior: Clip.hardEdge,
@@ -49,7 +67,7 @@ class DetailScreen extends StatelessWidget {
                         )
                       ]),
                   child: Image.network(
-                    thumb,
+                    widget.thumb,
                     headers: const {
                       'Referer': 'https://comic.naver.com',
                     },
